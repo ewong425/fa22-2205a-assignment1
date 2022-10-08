20b197b2aa0b55410cbd736804811d309cdf9bd8
Ethan Wong <ewong425@uwo.ca>
2022-10-08 00:24:01 -0400

    added comments

--- ewong425_LabAssignment1/src/LA1Q1/DemoSinglyLinkedList.java
+++ ewong425_LabAssignment1/src/LA1Q1/DemoSinglyLinkedList.java
@@ -18,36 +18,42 @@ public class DemoSinglyLinkedList {
         //Print out each arrayList
         System.out.println("\n" + intList);
         System.out.println("\n" + strList);
-        //create a Linked List for the integers and store each integer value in them
+        //create a Linked List for the integers and store each integer value in them from the ArrayList
         SinglyLinkList<Integer> numList = new SinglyLinkList<>();
         numList.addFirst(intList.get(0));
         numList.addFirst(intList.get(1));
         numList.addFirst(intList.get(2));
         numList.addLast(intList.get(4));
         numList.addLast(intList.get(3));
-        numList.toString();
+        numList.toString(); //return the Integer Linked List as a string
+        //create a linked list for the Strings and store each string value in them from the ArrayList
         SinglyLinkList<String> nameList = new SinglyLinkList<>();
         nameList.addFirst(strList.get(0));
         nameList.addFirst(strList.get(1));
         nameList.addFirst(strList.get(2));
         nameList.addLast(strList.get(4));
         nameList.addLast(strList.get(3));
-        nameList.toString();
+        nameList.toString(); //return the Integer Linked List as a string
+        //calling stackDemo to realize Linked List as a stack
         System.out.println("Stack demo with the Integer linked list.. \n");
         stackDemo(numList);
+        //calling queueDemo to realize Linked List as a queue
         System.out.println("Queue demo with the String linked list.. \n");
         queueDemo(nameList);
+        //Stack search section, while the input of the user is 'y'
+        System.out.println("Let's search the stack");
+        System.out.println("The current stack is: " + numList.toString());
         while(cont == 'y') {
+            //take in the value that the user wishes to be found
             try {
-                System.out.println("Let's search the stack");
-                System.out.println("The current stack is: " + numList.toString());
                 System.out.print("Enter the value you are searching for: ");
                 num = input.nextInt();
-            } catch(IndexOutOfBoundsException e) {
+            } catch(IndexOutOfBoundsException e) { //catch any index out of bounds
                 System.out.println("Index not found! Do you wish to continue? (enter 'y' for yes and any other key to exit.");
             }
+            //call searchStack method with their input
             numList.searchStack(num);
-            cont = input.next().charAt(0);
+            cont = input.next().charAt(0); //ask if they would like to continue searching
         }
         footer(1);
     }
